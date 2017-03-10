0c8f68f1e6607168a75753d10da3e3de3e288e93
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-10 11:32:40 +0000

    added aditional tests for both multiples

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,15 @@
 require './lib/fizzbuzz'
 
 describe Fixnum do
-  it 'returns "fizz" for the number 3' do
-    expect(3.fizzbuzz).to eq 'fizz'
+  it 'returns "fizz" for any multiple of 3' do
+    expect(9.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "buzz" for any multiple of 5' do
+    expect(25.fizzbuzz).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" for multiples of both 3 and 5' do
+    expect(30.fizzbuzz).to eq 'fizzbuzz'
   end
 end
